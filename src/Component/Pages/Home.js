b3a5efcfd92a28c5c8480ff34bb0b95a0a51870a
Rvdnotes <95022911+Rvdnotes@users.noreply.github.com>
2022-07-28 17:44:45 +0530

    homenew

--- src/Component/Pages/Home.js
+++ src/Component/Pages/Home.js
@@ -33,8 +33,7 @@ function App() {
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  some changes occured in the method of git source.
                 </p>
                 {/* <a href="#" className="btn btn-primary"> */}
                   Go somewhere
